adf7cca25d4fde5cfa2084d81406f4834499ba8d
Luigi Morel <morelmiles@gmail.com>
2021-11-03 17:55:55 +0300

    Add Main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,11 +2,31 @@ import React from "react";
 
 //Components
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/Hero";
+import FeaturedUniversities from "../components/Featured";
+import Services from "../components/Services";
+import News from "../components/News";
+import Events from "../components/Events";
+import Countries from "../components/Countries";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
+import CTA from "../components/CTA";
 
 const Main = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="bg-main-bg ">
+      <div className="sm:mx-14 mx-6 ">
+        <Navbar />
+        <HeroSection />
+      </div>
+      <FeaturedUniversities />
+      <Services />
+      <News />
+      <Events />
+      <Countries />
+      <Testimonials />
+      <CTA />
+      <Footer />
     </div>
   );
 };
